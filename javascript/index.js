dabf769a6a86775dc4b4aa521201d47345a24e06
040975 <vidalchri62@gmail.com>
2016-11-21 16:22:54 +0100

    ajout des fichiers

---
+++ javascript/index.js
@@ -0,0 +1,74 @@
+ var maison = prompt("bonjours quel est ton prenom" );
+  var maison1 = document.getElementById("maison1");
+   maison1.innerHTML = maison;
+   maison1.style.fontSize ="30px";
+
+
+
+   var heros = prompt("peut tu me donnez un nom  de chien ");
+   var heros0 = document.getElementsByClassName("heros1")[0];
+   var heros1 = document.getElementsByClassName("heros1")[1];
+   var heros2 = document.getElementsByClassName("heros1")[2];
+   var heros3 = document.getElementsByClassName("heros1")[3];
+   var heros4 = document.getElementsByClassName("heros1")[4];
+   var heros5 = document.getElementsByClassName("heros1")[5];
+   var heros6 = document.getElementsByClassName("heros1")[6];
+   var heros7 = document.getElementsByClassName("heros1")[7];
+   heros0.innerHTML = heros;
+   heros1.innerHTML = heros;
+   heros2.innerHTML = heros;
+   heros3.innerHTML = heros;
+   heros4.innerHTML = heros;
+   heros5.innerHTML = heros;
+   heros6.innerHTML = heros;
+   heros7.innerHTML = heros;
+
+
+   var couleur = prompt("peut tu me donner une couleur pour un chien");
+   var couleur1 = document.getElementById("couleur1");
+   couleur1.innerHTML = couleur;
+    couleur1.style.fontSize ="30px";
+
+
+
+    var oreille = prompt(" une couleur de l'oreille gauche");
+    var oreille2 = document.getElementById("gauche");
+    oreille2.innerHTML = oreille;
+    oreille2.style.fontSize ="30px";
+
+    var oreille1 = prompt(" une couleur  de l'oreille droite");
+    var oreille3 = document.getElementById("droite");
+    oreille3.innerHTML = oreille1;
+    oreille3.style.fontSize ="30px";
+
+    var queue = prompt(" ca couleur  de  queue");
+    var queue1 = document.getElementById("queue");
+    queue1.innerHTML = queue;
+     queue1.style.fontSize ="30px";
+
+    var race = prompt("donne moi une race de chien");
+    var race1 = document.getElementById("race");
+    race1.innerHTML = race;
+     race1.style.fontSize ="30px";
+
+
+
+    var ami1 = prompt("nom d'un ami");
+    var amis0 = document.getElementsByClassName("amis")[0];
+    var amis1 = document.getElementsByClassName("amis")[1];
+    var amis2 = document.getElementsByClassName("amis")[2];
+    var amis3 = document.getElementsByClassName("amis")[3];
+    amis0.innerHTML = ami1;
+    amis1.innerHTML = ami1;
+    amis2.innerHTML = ami1;
+    amis3.innerHTML = ami1;
+
+
+
+
+    var amis3 = prompt("nom d'une amie");
+    var amis4 = document.getElementsByClassName("amis2")[0];
+    var amis5 = document.getElementsByClassName("amis2")[1];
+    amis4.innerHTML = amis3;
+    amis5.innerHTML =amis3;
+    //amis3.style.display = "block";
